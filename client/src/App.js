51aa70c5b3102e37fe3b0d2f8bee46cbc3ba95bf
Valerii Logvin <valerii.logvin@gmail.com>
2021-04-26 15:59:07 +0300

    react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from "./routes"
 import 'materialize-css'
 
 
-
 function App() {
+    const routes = useRoutes(false)
     return (
-        <div className="container">
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div className="container">
+                {routes}
+            </div>
+        </Router>
     );
 }
 

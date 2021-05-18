a905d87aec8492c91dafec8fd2b2f93790800a58
Valerii Logvin <valerii.logvin@gmail.com>
2021-05-18 16:50:33 +0300

    link-page, links list

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -18,7 +18,7 @@ export const Navbar = () => {
                 <span className="brand-logo">Сокращение ссылок</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to="/create">Создать</NavLink></li>
-                    <li><NavLink to="/links">Создать</NavLink></li>
+                    <li><NavLink to="/links">Ссылки</NavLink></li>
                     <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
                 </ul>
             </div>

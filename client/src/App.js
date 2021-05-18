f3c3459aba48b62a81c512391e0fc6aec4eb1b36
Valerii Logvin <valerii.logvin@gmail.com>
2021-05-18 11:40:12 +0300

    link-detail page

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,20 @@ import {AuthContext} from "./context/AuthContext"
 import {useRoutes} from "./routes"
 import {useAuth} from "./hooks/auth.hook"
 import {Navbar} from "./components/Navbar"
+import {Loader} from "./components/Loader"
 
 import 'materialize-css'
 
 
 function App() {
-    const {token, login, logout, userId} = useAuth()
+    const {token, login, logout, userId, ready} = useAuth()
     const isAuthenticated = !!token
     const routes = useRoutes(isAuthenticated)
+
+    if(!ready){
+        return <Loader />
+    }
+
     return (
         <AuthContext.Provider value={{
             token, login, logout, userId, isAuthenticated
